5f163ca6aa8305140b3e6a4eb41a83f21076340e
Hara <harachung90@gmail.com>
2022-12-17 16:51:04 +1100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import './App.css';
+import React from 'react';
+import user from './images/girl.png';
+import emptyStar from './images/star-empty.png';
+import filledStar from './images/star-filled.png';
+
+function App() {
+
+    const [contact, setContact] = React.useState({
+        firstName: "Jane",
+        lastName: "Doe",
+        phone: "+1 (719) 555-1212",
+        email: "itsmyrealname@example.com",
+        isFavorite: false
+    })
+
+    function toggleFavorite() {
+        //console.log("Toggle favorite")
+        setContact(
+            {
+                firstName: contact.firstName,
+                lastName: contact.lastName,
+                phone: contact.phone,
+                email: contact.email,
+                isFavorite: !contact.isFavorite,
+            }
+        )
+    }
+
+    return (
+        <main>
+            <article className="card">
+                <img src={user} className="card--image"/>
+                <div className="card--info">
+                    <img
+                        src={contact.isFavorite ? filledStar : emptyStar}
+                        className="card--favorite"
+                        onClick={toggleFavorite}
+                    />
+                    <h2 className="card--name">
+                        {contact.firstName} {contact.lastName}
+                    </h2>
+                    <p className="card--contact">{contact.phone}</p>
+                    <p className="card--contact">{contact.email}</p>
+                </div>
+
+            </article>
+        </main>
+    );
+}
+
+export default App;
